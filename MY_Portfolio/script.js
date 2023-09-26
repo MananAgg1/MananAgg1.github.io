f798b5212901e63f167d355955c2f14941f32638
Manan <118235844+MananAgg1@users.noreply.github.com>
2023-09-27 00:16:46 +0530

    Add files via upload

---
+++ MY_Portfolio/script.js
@@ -0,0 +1,21 @@
+
+
+function bigImg1(x) {
+    x.style.height = "520px";
+    x.style.width = "500px";
+}
+  
+function normalImg1(x) {
+    x.style.height = "500px";
+    x.style.width = "500px";
+}
+
+function bigImg2(x) {
+    x.style.height = "520px";
+    x.style.width = "1000px";
+}
+
+function normalImg2(x) {
+    x.style.height = "500px";
+    x.style.width = "1000px";
+}
